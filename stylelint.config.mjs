50f206a86651f4f46242feb794dddbd6e77495dc
Ömer Balyalı <git@omerbalyali.com>
2026-05-10 02:16:47 +0200

    chore: change color notation
    Signed-off-by: Ömer Balyalı <git@omerbalyali.com>

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,6 +2,9 @@
 export default {
 	extends: ["stylelint-config-standard", "stylelint-config-html/astro"],
 	rules: {
+		"lightness-notation": "number",
+		"hue-degree-notation": "number",
+		"alpha-value-notation": "number",
 		"custom-property-empty-line-before": null,
 		"number-max-precision": 5,
 		"value-keyword-case": [
