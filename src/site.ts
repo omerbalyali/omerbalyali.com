bc526bf824885338ab52e21cb6be110f7d745215
Ömer Balyalı <git@omerbalyali.com>
2026-05-13 00:00:16 +0200

    feat: change site url
    Signed-off-by: Ömer Balyalı <git@omerbalyali.com>

--- src/site.ts
+++ src/site.ts
@@ -1,6 +1,6 @@
 export const SITE = {
 	name: "Ömer Balyalı",
-	url: "https://preview.omerbalyali.com",
+	url: "https://omerbalyali.com",
 	description: "Personal website of Ömer Balyalı.",
 	locale: "en_US",
 	themeColor: "#fff",
